4d18b99f22efbc89c336e6faa06f697c8b43f052
chuong nguyen <nnchuong411@gmail.com>
2015-12-25 17:49:42 +0700

    Container: register offical close to shutdown hook;

--- src/main/java/home/spring/ioc/exercises/second/Main.java
+++ src/main/java/home/spring/ioc/exercises/second/Main.java
@@ -1,16 +1,17 @@
 package home.spring.ioc.exercises.second;
 
-import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
-        AbstractApplicationContext context = new ClassPathXmlApplicationContext("second/spring.xml");
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("second/spring.xml");
+
+        context.registerShutdownHook();
 
         Object bean = context.getBean("underExaminingBean", UnderExaminingBean.class);
 
-        context.close();
 
     }
 }
